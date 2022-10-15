faa3b5b81b80803ccb2eeb493db76e9359fadd58
Angelica Lopez <angelica.lopezcap@gmail.com>
2022-10-15 17:32:05 -0500

    Adding components

--- burger-crunch/src/app/app.module.ts
+++ burger-crunch/src/app/app.module.ts
@@ -3,10 +3,16 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { ChefComponent } from './components/chef/chef.component';
+import { LoginComponent } from './components/login/login.component';
+import { WaiterComponent } from './components/waiter/waiter.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    ChefComponent,
+    LoginComponent,
+    WaiterComponent
   ],
   imports: [
     BrowserModule,
---
+++ burger-crunch/src/app/recap.ts
@@ -0,0 +1,17 @@
+const username: string = 'nanukelguru';
+const sum = (a: number, b:number) => {
+  return a + b;
+}
+
+sum(1,3);
+
+class Person {
+
+ constructor(public age: number, public lastName: string) {}
+}
+
+const ronno = new Person(15, 'Lopez');
+ronno.age
+
+
+
